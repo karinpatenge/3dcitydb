5d0abc3f3d471568e328150cb2d8f36771eac58b
Karin Patenge <karin.patenge@oracle.com>
2025-09-26 10:51:14 +0200

    Create connection-details.sh
    Ready for review

---
+++ oracle/shell-scripts/unix/connection-details.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Provide your database details here ------------------------------------------
+export DBHOST=your_host_address
+export DBPORT=1521
+export ORACLE_PDB=your_pdb
+export CITYDB=your_database_schema
+export DBUSER=your_username
+#------------------------------------------------------------------------------
+
+# ToDo: Check if CITYDB and DBUSER are required
